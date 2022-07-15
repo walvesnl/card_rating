1c61e2a19ad40f7ddf4e8253564eaa2209e6ac14
w.alvesnl <w.alvesnl@gmail.com>
2022-07-15 14:44:25 +0200

    hide buttons on last quote

--- design1/app.js
+++ design1/app.js
@@ -12,16 +12,23 @@ function currentQuote(n) {
 function showQuotes(n) {
   let i;
   let quotes = document.getElementsByClassName("quotes");
+  let buttons = document.querySelector(".buttons");
+  let arrow = document.querySelector(".arrow-button");
 
   if (n > quotes.length) {
-    quoteIndex = 1;
+    quotesIndex = 1;
   }
   if (n < 1) {
-    quoteIndex = quotes.length;
+    quotesIndex = quotes.length;
   }
   for (i = 0; i < quotes.length; i++) {
     quotes[i].style.display = "none";
   }
 
   quotes[quotesIndex - 1].style.display = "block";
+
+  if (quotesIndex === 7) {
+    arrow.style.display = "none";
+    buttons.style.display = "none";
+  }
 }
