81fb960d0a970584063ac148933a53129e178f3f
w.alvesnl <w.alvesnl@gmail.com>
2022-07-15 16:50:33 +0200

    answer validation

--- design3/app.js
+++ design3/app.js
@@ -1,19 +1,45 @@
 let quotesIndex = 1;
 showQuotes(quotesIndex);
 
+let buttons = document.getElementsByClassName("button");
+
+let selected = [false, false, false];
+
 function nextQuote(n) {
+  if (!selected.includes(true)) {
+    return alert("You must choose at least one answer");
+  }
+
+  buttons[0].style.border = "2px solid black";
+  buttons[1].style.border = "2px solid black";
+  buttons[2].style.border = "2px solid black";
+
   showQuotes((quotesIndex += n));
+  selected = [false, false, false];
+  console.log("log in funct", selected);
 }
+
+console.log(selected);
 
 function currentQuote(n) {
   showQuotes((quoteIndex = n));
 }
 
+function selectedAnswer(n) {
+  selected[n] = !selected[n];
+  console.log(selected);
+
+  selected[n]
+    ? (buttons[n].style.border = "2px solid orange")
+    : (buttons[n].style.border = "2px solid black");
+}
+
 function showQuotes(n) {
   let i;
   let quotes = document.getElementsByClassName("quotes");
-  let buttons = document.querySelector(".buttons");
   let arrow = document.querySelector(".arrow-button");
+  let submit = document.querySelector(".submit");
+  let buttons = document.querySelector(".buttons");
 
   if (n > quotes.length) {
     quotesIndex = 1;
@@ -30,5 +56,6 @@ function showQuotes(n) {
   if (quotesIndex === 7) {
     arrow.style.display = "none";
     buttons.style.display = "none";
+    submit.style.display = "none";
   }
 }
